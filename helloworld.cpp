9957a1acb4858b77addb8eaa0d5338fa9e48380d
RMvirtual <rmvirtual@AOL.com>
2021-07-10 22:39:27 +0100

    More work on experimenting with classes.

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,12 +3,27 @@ using namespace std;
 
 class PrintableMessage
 {
-    public: string messageToPrint = "Hello World!";
+    public:
+        string message1 = "Hello World!";
+        string message2 = "Bonjour";
+    private:
+        string message3;
+
+    public: PrintableMessage(string privateMessage)
+    {
+        message3 = privateMessage;
+    }
+
+    public: string getMessage3()
+    {
+        return this->message3;
+    }
+
 };
 
 int main()
 {
-    PrintableMessage message = PrintableMessage();
-    cout << message.messageToPrint;
+    PrintableMessage message = PrintableMessage("Aight");
+    cout << message.getMessage3();
     return 0;
 }
