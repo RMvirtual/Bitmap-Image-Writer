9e71df0ecdf44351d76d3592fe5cf86287a96076
RMvirtual <rmvirtual@AOL.com>
2021-07-22 12:25:20 +0100

    Removed unneeded scripts.

--- script/compile_boost.bat
+++
@@ -1,8 +0,0 @@
-: Compile the boost binaries.
-
-@echo off
-cd ../lib/boost_1_76_0
-bootstrap.bat
-b2.exe
-@echo on
-
--- script/compile_tests.bat
+++
@@ -1,9 +0,0 @@
-: Compile the script in Windows.
-@echo off
-cls
-cd ../test
-
-g++ test.cpp -o ../build/test/test.exe -I ../lib/boost_1_76_0 
-
-cd ../script
-@echo on
