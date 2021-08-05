6947337c8ccac0b5cae2370ee57fa6f579959963
RMvirtual <rmvirtual@AOL.com>
2021-08-05 12:17:17 +0100

    Documentation for the vector.h header.

--- src/maths/vector/vector.h
+++ src/maths/vector/vector.h
@@ -10,26 +10,83 @@ namespace Vectorable
     class Vector
     {
         public:
+            
+            /**
+             * Creates a new vector with an array of doubles as the
+             * vector elements. Requires the length of the array to
+             * be passed in manually as second parameter due to the
+             * array C++ primitive not knowing it's length once passed
+             * to a function.
+             */
             Vector(double array[], int noOfArrayElements);
+
+            /**
+             * Creates a new vector using the parameters as vector
+             * points.
+             */
             Vector(double a, double b);
+            
+            /**
+             * Creates a new vector using the parameters as vector
+             * points.
+             */
             Vector(double a, double b, double c);
+            
+            /**
+             * Creates a new vector using the parameters as vector
+             * points.
+             */
             Vector(double a, double b, double c, double d);
 
-            double operator [] (int index);
-            Vector operator + (Vector righthandsideVector);
-            Vector operator - (Vector righthandsideVector);
-            double operator * (Vector righthandsideVector);
-            Vector operator * (double righthandsideScalar);
-            Vector operator / (double scalar);
-            string toString();
-
-            int length();
-            double magnitude();
+            /**
+             * Returns the scalar/dot product of a vector with another
+             * vector.
+             */
             double dotProduct(Vector vector);
+
+            // Returns the vector.
             Vector vectorProduct(Vector vector);
+
+            // Gets the vector element by index position.
             double get(int index);
 
-        private:            
+            // Returns the length of the vector.
+            int length();
+
+            // Returns the magnitude of the vector.
+            double magnitude();
+
+            // Indexes the elements contained within the vector.
+            double operator [] (int index);
+
+            /**
+             * Returns a new vector forming from the addition of this
+             * vector with another vector.
+             */
+            Vector operator + (Vector vectorRhs);
+
+            /**
+             * Returns a new vector forming from the difference of this
+             * vector with another vector.
+             */ 
+            Vector operator - (Vector vectorRhs);
+
+            /**
+             * Returns the dot product of this vector with another
+             * vector.
+             */
+            double operator * (Vector vectorRhs);
+
+            // Returns a new vector using scalar multiplication.
+            Vector operator * (double scalarRhs);
+
+            // Returns a new vector using scalar division.
+            Vector operator / (double scalar);
+
+            // Returns a string representation of the vector.
+            string toString();
+
+        private:
             int lengthOfVector;
             double *vectorArray;
             
@@ -45,5 +102,14 @@ namespace Vectorable
 
 using namespace Vectorable;
 
-Vector operator * (double scalarLHS, Vector vectorRHS);
+/**
+ * Overloaded * operator that allows multiplication between a scalar
+ * double on the LHS and a vector on the RHS.
+ */
+Vector operator * (double scalarLhs, Vector vectorRhs);
+
+/**
+ * Allows the vector to be printed to cout directly
+ * as an object with toString() needing to be called.
+ */
 ostream& operator << (ostream &_stream, Vector v);
