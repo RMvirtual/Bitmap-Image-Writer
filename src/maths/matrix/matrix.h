08474a7b10c4ebf92c9df175965778eddf07514b
RMvirtual <rmvirtual@AOL.com>
2021-08-16 17:36:42 +0100

    Refactored matrix header to implicitly call std::vector as vector and just have Maths::Vector show the namespace explicity.

--- src/maths/matrix/matrix.h
+++ src/maths/matrix/matrix.h
@@ -4,23 +4,25 @@
 #include <vector>
 #include "src/maths/vector/vector.h"
 
+using namespace std;
+
 namespace Maths
 {
   class Matrix
   {
     public:
-      // Returns an empty matrix to populate.
+      // Returns an empty matrix.
       Matrix();
 
       // Returns a matrix populated with one column of values.
-      Matrix(std::vector<double> columnValues);
+      Matrix(vector<double> columnValues);
       
       /**
        * Returns a matrix populated with multiple columns of values.
        * All rows in the vector must be the same size or an arithmetic
        * error with throw.
        */
-      Matrix(std::vector<std::vector<double>> columns);
+      Matrix(vector<vector<double>> columns);
 
       // Returns the width (number of columns) of the matrix.
       int width();
@@ -29,10 +31,10 @@ namespace Maths
       int height();
 
       // Returns an stl vector containing the values of a specific row.
-      std::vector<double> getRow(int index);
+      vector<double> getRow(int index);
 
       // Returns an stl vector containing the values of a specific column.
-      std::vector<double> getColumn(int index);
+      vector<double> getColumn(int index);
 
       /**
        * Returns a 2D stl vector containing single vectors all the
@@ -40,7 +42,7 @@ namespace Maths
        * each vector contains the values found in all the rows of a
        * single column).
        */
-      std::vector<std::vector<double>> getColumns();
+      vector<vector<double>> getColumns();
       
       /**
        * Returns a 2D stl vector containing single vectors of all the
@@ -48,14 +50,14 @@ namespace Maths
        * (i.e. each vector contains the values found in all the columns
        * of single row).
        */
-      std::vector<std::vector<double>> getRows();
+      vector<vector<double>> getRows();
 
       /**
        * Indexes an stl vector representing a column of values (can be
        * indexed again using the subscript operator to get the actual
        * row value.
        */
-      std::vector<double> operator [] (int index);
+      vector<double> operator [] (int index);
 
       // Multiplies the matrix by a vector.
       Maths::Vector operator * (Maths::Vector vectorRhs);
@@ -67,34 +69,34 @@ namespace Maths
       string toString();
 
     private:
-      std::vector<std::vector<double>> columns;
+      vector<vector<double>> columns;
 
       Matrix multiplyMatrix(Matrix matrix);
       
-      std::vector<std::vector<double>> getValuesFromMatrixMultiplication(
+      vector<vector<double>> getValuesFromMatrixMultiplication(
         Matrix matrix);
 
-      std::vector<double> getProductAgainstAllRowsWithColumn(
-        std::vector<double> column);
+      vector<double> getProductAgainstAllRowsWithColumn(
+        vector<double> column);
 
       double getProductBetweenTwoStlVectors(
-        std::vector<double> vector1, std::vector<double> vector2);
+        vector<double> vector1, vector<double> vector2);
 
       double getProductBetweenTwoStlVectorsAtSameIndex(
-        std::vector<double> vector1, std::vector<double> vector2,
+        vector<double> vector1, vector<double> vector2,
         int commonIndex
       );
       
       void convert2DStlVectorToColumns(
-        std::vector<std::vector<double>> vector);
+        vector<vector<double>> vector);
 
       bool check2DStlVectorIsValidMatrixSize(
-        std::vector<std::vector<double>> vector);
+        vector<vector<double>> vector);
         
       bool isVectorMultipliable(Maths::Vector vector);
       bool isMatrixMultipliable(Matrix matrix);
       string getAllValuesAsString();
-      string formatStlVectorAsMatrixRow(std::vector<double> vector);
+      string formatStlVectorAsMatrixRow(vector<double> vector);
   };
 }
 
