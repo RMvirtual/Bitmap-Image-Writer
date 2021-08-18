145d49bd97ab4b38a45a1f37735c7400246c57fe
RMvirtual <rmvirtual@AOL.com>
2021-08-18 23:56:05 +0100

    Debugging information for struct sizes added to figure out potential padding issue with bmp creating.

--- src/graphics/bitmap.h
+++ src/graphics/bitmap.h
@@ -3,35 +3,38 @@
 
 using namespace std;
 
-class BitmapHeader
+// 14 bytes total.
+struct BitmapHeader
 {
-public:
-  char bitmapSignatureBytes[2] = {'B', 'M'};
-  uint32_t sizeOfBitmapFile = 54 + 786432;
-  uint32_t reservedBytes = 0;
-  uint32_t pixelDataOffset = 54;
+  char bitmapSignatureBytes[2] = {'B', 'M'}; // 2 bytes
+  uint32_t sizeOfBitmapFile = 54 + 786432; // 4 byte
+  uint32_t reservedBytes = 0; // 4 byte
+  uint32_t pixelDataOffset = 54; // 4 byte
 };
 
-class BitmapInfoHeader
+// 40 bytes total.
+struct BitmapInfoHeader
 {
-public:
-  uint32_t sizeOfThisHeader = 40;
-  int32_t widthInPixels = 512;
-  int32_t heightInPixels = 512; 
-  uint16_t numberOfColorPlanes = 1; // must be 1?
-  uint16_t colorDepth = 24;
-  uint32_t compressionMethod = 0;
-  uint32_t rawBitmapDataSize = 0; // generally ignored?
-  int32_t horizontalResolutionPixelPerMeter = 3780;
-  int32_t verticalResolutionPixelsPerMeter = 3780;
-  uint32_t colorTableEntries = 0;
-  uint32_t importantColors = 0;
+  uint32_t sizeOfThisHeader = 40; // 4 byte
+  int32_t widthInPixels = 512; // 4 byte
+  int32_t heightInPixels = 512; // 4 byte
+  uint16_t numberOfColorPlanes = 1; // must be 1? // 2 bytes
+  uint16_t colorDepth = 24; // 2 bytes
+  uint32_t compressionMethod = 0; // 4 bytes 
+  uint32_t rawBitmapDataSize = 0; // generally ignored? // 4 bytes
+  int32_t horizontalResolutionPixelPerMeter = 3780; // 4 bytes
+  int32_t verticalResolutionPixelsPerMeter = 3780; // 4 bytes
+  uint32_t colorTableEntries = 0; // 4 bytes
+  uint32_t importantColors = 0; // 4 bytes
 };
 
-class Pixel
+// 54 bytes total.
+
+// 3 bytes total?
+struct Pixel
 {
-public:
   uint8_t blue = 100;
   uint8_t green = 100;
   uint8_t red = 100;
+  uint8_t padMe = 0;
 };
--- test/src/graphics-tests/bitmap_tests.cpp
+++ test/src/graphics-tests/bitmap_tests.cpp
@@ -9,6 +9,10 @@ TEST(GraphicsTests, ShouldCreateBitmap)
   BitmapInfoHeader bmpInfoHeader;
   Pixel pixel;
 
+  cout << "Size of bmpHeader: " + to_string(sizeof(bmpHeader)) << endl;
+  cout << "Size of bmpInfoHeader: " + to_string(sizeof(bmpInfoHeader)) << endl;
+  cout << "Size of pixel: " + to_string(sizeof(pixel)) << endl;
+
   char* outputPath = "C:\\Users\\rmvir\\Desktop\\scc300-Win3D\\test\\output\\myImage.bmp";
   ofstream fout(outputPath, ios::binary);
 
@@ -19,7 +23,7 @@ TEST(GraphicsTests, ShouldCreateBitmap)
   size_t numberOfPixels = bmpInfoHeader.widthInPixels * bmpInfoHeader.heightInPixels;
   
   for (int i = 0; i < numberOfPixels; i++)
-    fout.write((char *) &pixel, 3);
+    fout.write((char *) &pixel, 4);
   
   fout.close();
 }
