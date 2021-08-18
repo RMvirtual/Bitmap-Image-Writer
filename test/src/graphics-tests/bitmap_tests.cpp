1fd1a8c519d52a833d4806a936b41de386a46c3e
RMvirtual <rmvirtual@AOL.com>
2021-08-18 23:17:14 +0100

    Work on output of a bitmap file (although wrong colour at the moment so may be problem with padding).

--- test/src/graphics-tests/bitmap_tests.cpp
+++ test/src/graphics-tests/bitmap_tests.cpp
@@ -1,9 +1,25 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include <string>
 #include "src/graphics/bitmap.h"
 
 TEST(GraphicsTests, ShouldCreateBitmap)
 {
   BitmapHeader bmpHeader;
+  BitmapInfoHeader bmpInfoHeader;
+  Pixel pixel;
+
+  std::cout << bmpHeader.bitmapSignatureBytes[0] << endl;
+  ofstream fout("test_bitmap.bmp", ios::binary);
+
+  fout.write((char *) &bmpHeader, 14);
+  fout.write((char *) &bmpInfoHeader, 40);
+
+  // writing pixel data
+  size_t numberOfPixels = bmpInfoHeader.widthInPixels * bmpInfoHeader.heightInPixels;
   
+  for (int i = 0; i < numberOfPixels; i++)
+    fout.write((char *) &pixel, 3);
+  
+  fout.close();
 }
