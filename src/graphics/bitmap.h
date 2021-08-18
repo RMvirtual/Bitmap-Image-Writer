c17122d591c68662f14980d1555f6ea353cb685b
RMvirtual <rmvirtual@AOL.com>
2021-08-18 23:05:28 +0100

    Start of graphics library and tests.

--- src/graphics/bitmap.h
+++ src/graphics/bitmap.h
@@ -3,11 +3,35 @@
 
 using namespace std;
 
-class BmpHeader
+class BitmapHeader
 {
 public:
-    char bitmapSignatureBytes[2] = {'B', 'M'};
-    uint32_t sizeOfBitmapFile = 54 + 786432;
-    uint32_t reservedBytes = 0;
-    uint32_t pixelDataOffset = 54;
+  char bitmapSignatureBytes[2] = {'B', 'M'};
+  uint32_t sizeOfBitmapFile = 54 + 786432;
+  uint32_t reservedBytes = 0;
+  uint32_t pixelDataOffset = 54;
+};
+
+class BitmapInfoHeader
+{
+public:
+  uint32_t sizeOfThisHeader = 40;
+  int32_t widthInPixels = 512;
+  int32_t heightInPixels = 512; 
+  uint16_t numberOfColorPlanes = 1; // must be 1?
+  uint16_t colorDepth = 24;
+  uint32_t compressionMethod = 0;
+  uint32_t rawBitmapDataSize = 0; // generally ignored?
+  int32_t horizontalResolutionPixelPerMeter = 3780;
+  int32_t verticalResolutionPixelsPerMeter = 3780;
+  uint32_t colorTableEntries = 0;
+  uint32_t importantColors = 0;
+};
+
+class Pixel
+{
+public:
+  uint8_t blue = 255;
+  uint8_t green = 255;
+  uint8_t red = 0;
 };
---
+++ test/src/graphics-tests/bitmap_tests.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include "src/graphics/bitmap.h"
+
+TEST(GraphicsTests, ShouldCreateBitmap)
+{
+  BitmapHeader bmpHeader;
+  
+}
