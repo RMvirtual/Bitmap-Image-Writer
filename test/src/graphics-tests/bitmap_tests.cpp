cab731b1b4d7c80d1513e36e10c383819ac2f38a
RMvirtual <rmvirtual@AOL.com>
2021-08-19 11:35:21 +0100

    Removed duplicate bytes array.

--- test/src/graphics-tests/bitmap_tests.cpp
+++ test/src/graphics-tests/bitmap_tests.cpp
@@ -20,8 +20,7 @@ TEST(GraphicsTests, ShouldCreateBitmap)
   char* bytes = bmpHeader.toBytes();
   cout << "Printing bytes: " << *bytes << endl;
 
-  fout.write(bmpHeader.toBytes(), 6);
-  fout.write((char *) &bmpHeaderStruct.reservedBytes, 4);
+  fout.write(bytes, 10);
   fout.write((char *) &bmpHeaderStruct.pixelDataOffset, 4);
   fout.write((char *) &bmpInfoHeader, 40);
 
