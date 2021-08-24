7028b605427540e55ba05ce943dfe3be3510179b
RMvirtual <rmvirtual@AOL.com>
2021-08-24 17:19:36 +0100

    Passing test for adding multiple uint32 to ByteArray class.

--- test/src/common-tests/byte_array_tests.cpp
+++ test/src/common-tests/byte_array_tests.cpp
@@ -3,6 +3,8 @@
 
 #include "src/common/byte_array.h"
 
+using namespace std;
+
 TEST(ByteArrayTests, ShouldCreateByteArray)
 {
   ByteArrayBuilder byteArrayBuilder;
@@ -30,5 +32,35 @@ TEST(ByteArrayTests, ShouldAddUint32ToArray)
   for (int i = 0; i < 4; i++) {
     ASSERT_EQ(correctByteArray[i], byteArray[i]);
   }
+}
 
+TEST(ByteArrayTests, ShouldAddMultipleUint32ToArray)
+{
+  ByteArrayBuilder byteArrayBuilder;
+  uint32_t correctValue1 = 20;
+  uint32_t correctValue2 = 40;
+  uint32_t correctValue3 = 45318294;
+
+  byteArrayBuilder.addFourByteValueToCharArray(correctValue1);
+  byteArrayBuilder.addFourByteValueToCharArray(correctValue2);
+  byteArrayBuilder.addFourByteValueToCharArray(correctValue3);
+
+  char* byteArray = byteArrayBuilder.toArray();
+
+  char* correctByteArray = new char[12];
+  correctByteArray[0] = correctValue1,
+  correctByteArray[1] = correctValue1 >> 8;
+  correctByteArray[2] = correctValue1 >> 16;
+  correctByteArray[3] = correctValue1 >> 32;
+  correctByteArray[4] = correctValue2,
+  correctByteArray[5] = correctValue2 >> 8;
+  correctByteArray[6] = correctValue2 >> 16;
+  correctByteArray[7] = correctValue2 >> 32;
+  correctByteArray[8] = correctValue3,
+  correctByteArray[9] = correctValue3 >> 8;
+  correctByteArray[10] = correctValue3 >> 16;
+  correctByteArray[11] = correctValue3 >> 32;
+
+  for (int i = 0; i < 12; i++)
+    ASSERT_EQ(correctByteArray[i], byteArray[i]);
 }
