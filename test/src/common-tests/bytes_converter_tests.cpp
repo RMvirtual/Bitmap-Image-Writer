d633993062cb628e189460eab73bd2a9a901473d
RMvirtual <rmvirtual@AOL.com>
2021-08-26 21:20:22 +0100

    BytesConverter: Tests added covering the other conversion from string to bytes functions.

--- test/src/common-tests/bytes_converter_tests.cpp
+++ test/src/common-tests/bytes_converter_tests.cpp
@@ -6,6 +6,32 @@
 
 using namespace std;
 
+TEST(BytesConverterTest, ShouldGetTwoBytesFromSubstring)
+{
+  BytesConverter bytesConverter;
+  string stringToConvert = "crym3@river";
+
+  uint32_t convertedValue 
+    = bytesConverter.getTwoBytesFromSubstring(stringToConvert, 3);
+
+  uint32_t correctValue = 13165;
+
+  EXPECT_DOUBLE_EQ(correctValue, convertedValue);  
+}
+
+TEST(BytesConverterTest, ShouldGetFourBytesFromSubstring)
+{
+  BytesConverter bytesConverter;
+  string stringToConvert = "crym3@river";
+
+  uint32_t convertedValue 
+    = bytesConverter.getFourBytesFromSubstring(stringToConvert, 3);
+
+  uint32_t correctValue = 1916810093;
+
+  EXPECT_DOUBLE_EQ(correctValue, convertedValue);  
+}
+
 TEST(BytesConverterTest, ShouldConvertFourCharactersTo32bitInteger)
 {
   BytesConverter bytesConverter;
