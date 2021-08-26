d884d5ba9901a04fdca63869f7a3e3cc95860de3
RMvirtual <rmvirtual@AOL.com>
2021-08-26 21:31:31 +0100

    BinaryOps: Reorganised order of definitions in source file.

--- src/maths/binary_ops/binary_ops.cpp
+++ src/maths/binary_ops/binary_ops.cpp
@@ -2,26 +2,9 @@
 
 using namespace BinaryOps;
 
-BinaryOperation::BinaryOperation()
-{
-  // Nothing else to do here.
-}
-
-Addition::Addition()
-: BinaryOperation()
-{
-  // Nothing else to do.
-}
-
 double Addition::perform(double left, double right)
 {
   return left + right;
-}
-
-Subtraction::Subtraction()
-: BinaryOperation()
-{
-  // Nothing else to do.
 }
 
 double Subtraction::perform(double left, double right)
@@ -29,24 +12,41 @@ double Subtraction::perform(double left, double right)
   return left - right;
 }
 
-Multiplication::Multiplication()
-: BinaryOperation()
-{
-  // Nothing else to do.
-}
-
 double Multiplication::perform(double left, double right)
 {
   return left * right;
 }
 
-Division::Division()
-: BinaryOperation()
-{
-  // Nothing else to do.
-}
-
 double Division::perform(double left, double right)
 {
   return left / right;
+}
+
+BinaryOperation::BinaryOperation()
+{
+  // Pass.
+}
+
+Addition::Addition()
+: BinaryOperation()
+{
+  // Pass.
+}
+
+Subtraction::Subtraction()
+: BinaryOperation()
+{
+  // Pass.
+}
+
+Multiplication::Multiplication()
+: BinaryOperation()
+{
+  // Pass.
+}
+
+Division::Division()
+: BinaryOperation()
+{
+  // Pass.
 }
