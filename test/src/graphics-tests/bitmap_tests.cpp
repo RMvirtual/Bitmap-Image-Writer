125b641edba2d52281851043724d534f52e18f1f
RMvirtual <rmvirtual@AOL.com>
2021-08-28 13:08:30 +0100

    Refactored ShouldCreateBitmap test to make more sense of the PixelArray class.

--- test/src/graphics-tests/bitmap_tests.cpp
+++ test/src/graphics-tests/bitmap_tests.cpp
@@ -19,6 +19,14 @@ TEST(GraphicsTests, ShouldCreateBitmap)
   Pixel pixel {100, 255, 255};
   Pixel blackPixel {0, 0, 0};
 
+  size_t numberOfPixels = widthInPixels * heightInPixels;
+  std::vector<Pixel> pixelsToAdd;
+
+  for (int i = 0; i < numberOfPixels; i++)
+    pixelsToAdd.push_back(pixel);
+
+  PixelArray pixelArray {pixelsToAdd, widthInPixels, heightInPixels};
+
   char* outputPath = 
     "C:\\Users\\rmvir\\Desktop\\scc300-Win3D\\test\\output\\myImage.bmp";
 
@@ -29,26 +37,18 @@ TEST(GraphicsTests, ShouldCreateBitmap)
   
   fout.write(bmpFileHeaderBytes, bmpFileHeader.getSizeOfHeaderInBytes());
   fout.write(bmpInfoHeaderBytes, bmpInfoHeader.getSizeOfHeaderInBytes());
-
-  delete[] bmpFileHeaderBytes;
-  delete[] bmpInfoHeaderBytes;
-
-  size_t numberOfPixels = widthInPixels * heightInPixels;
   
-  for (int i = 0; i < numberOfPixels; i++) {
+  for (auto pixel : pixelArray.pixels) {
     char* pixelBytes = pixel.toBytes();
     fout.write(pixelBytes, 3);
 
     delete[] pixelBytes;
   }
 
-  fout.close();
+  delete[] bmpFileHeaderBytes;
+  delete[] bmpInfoHeaderBytes;
 
-  short int word = 0x0001;
-  char *byte = (char *) &word;
-  
-  std::string string1 = byte[0] ? "Little" : "big";
-  std::cout << string1 << std::endl;
+  fout.close();
 }
 
 TEST(GraphicsTests, ShouldGetBitmapLength)
