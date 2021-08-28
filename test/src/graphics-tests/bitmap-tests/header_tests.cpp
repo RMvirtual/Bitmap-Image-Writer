baf4a12cef3c7a85503863c76cd2260593af8ce2
RMvirtual <rmvirtual@AOL.com>
2021-08-28 17:38:00 +0100

    Refactored more of the bitmap tests into reader tests.

--- test/src/graphics-tests/bitmap-tests/header_tests.cpp
+++ test/src/graphics-tests/bitmap-tests/header_tests.cpp
@@ -10,7 +10,7 @@
 #include "src/graphics/bitmaps/reader/bitmap_reader.h"
 #include "src/common/filesystem.h"
 
-TEST(BitmapTests, ShouldCreateBitmap)
+TEST(BitmapWriterTests, ShouldCreateBitmap)
 {
   int widthInPixels = 512, heightInPixels = 512;
 
@@ -48,57 +48,4 @@ TEST(BitmapTests, ShouldCreateBitmap)
   delete[] bmpInfoHeaderBytes;
 
   fout.close();
-
-
-}
-
-TEST(BitmapTests, ShouldGetBitmapFileSize)
-{ 
-  char* bitmapFile = (
-    "C:\\Users\\rmvir\\Desktop\\scc300-Win3D\\test\\output\\" \
-    "correct_resources\\blueImage512x512.bmp"
-  );
-
-  int correctSize = 786486;
-  int fileSizeInBytes = Filesystem::getSizeOfFile(bitmapFile);
-
-  EXPECT_EQ(correctSize, fileSizeInBytes);
-}
-
-TEST(BitmapTests, ShouldGetBitmapFileHeader)
-{ 
-  char* bmpFile = (
-    "C:\\Users\\rmvir\\Desktop\\scc300-Win3D\\test\\output\\" \
-    "correct_resources\\blueImage512x512.bmp"
-  );
-
-  BitmapFileHeader bmpFileHeader = BitmapReader::getBitmapFileHeader(bmpFile);
-
-  EXPECT_EQ('B', bmpFileHeader.getSignatureBytes()[0]);
-  EXPECT_EQ('M', bmpFileHeader.getSignatureBytes()[1]);  
-  EXPECT_EQ("BM", bmpFileHeader.getSignatureBytes());
-  EXPECT_EQ(786486, bmpFileHeader.getSizeOfBitmapFile());
-  EXPECT_EQ(0, bmpFileHeader.getReservedBytes());
-  EXPECT_EQ(54, bmpFileHeader.getPixelDataOffset());
-}
-
-TEST(BitmapTests, ShouldGetBitmapInfoHeader)
-{
-  char* bmpFile = (
-    "C:\\Users\\rmvir\\Desktop\\scc300-Win3D\\test\\output\\" \
-    "correct_resources\\blueImage512x512.bmp"
-  );
-
-  BitmapDibHeader bmpInfoHeader = BitmapReader::getBitmapDibHeader(bmpFile);
-
-  EXPECT_EQ(40, bmpInfoHeader.getSizeOfHeaderInBytes());
-  EXPECT_EQ(512, bmpInfoHeader.getWidthInPixels());
-  EXPECT_EQ(512, bmpInfoHeader.getHeightInPixels());
-  EXPECT_EQ(1, bmpInfoHeader.getNumberOfColorPlanes());
-  EXPECT_EQ(0, bmpInfoHeader.getCompressionMethod());
-  EXPECT_EQ(0, bmpInfoHeader.getRawBitmapDataSize());
-  EXPECT_EQ(0, bmpInfoHeader.getHorizontalPixelsPerMetre());
-  EXPECT_EQ(0, bmpInfoHeader.getVerticalPixelsPerMetre());
-  EXPECT_EQ(0, bmpInfoHeader.getColorTableEntries());
-  EXPECT_EQ(0, bmpInfoHeader.getImportantColors());
 }
--- test/src/graphics-tests/bitmap-tests/reader_tests.cpp
+++ test/src/graphics-tests/bitmap-tests/reader_tests.cpp
@@ -1,11 +1,46 @@
 #include <gtest/gtest.h>
-#include <vector>
 
-#include "src/graphics/bitmaps/packet/pixels/pixel_array.h"
-#include "src/graphics/bitmaps/packet/pixels/pixel.h"
 #include "src/graphics/bitmaps/reader/bitmap_reader.h"
 
-TEST(BitmapTests, ShouldGetPixelArraySizeFromHeader)
+TEST(BitmapReaderTests, ShouldExtractBitmapFileHeaderFromImage)
+{ 
+  char* bmpFile = (
+    "C:\\Users\\rmvir\\Desktop\\scc300-Win3D\\test\\output\\" \
+    "correct_resources\\blueImage512x512.bmp"
+  );
+
+  BitmapFileHeader bmpFileHeader = BitmapReader::getBitmapFileHeader(bmpFile);
+
+  EXPECT_EQ('B', bmpFileHeader.getSignatureBytes()[0]);
+  EXPECT_EQ('M', bmpFileHeader.getSignatureBytes()[1]);  
+  EXPECT_EQ("BM", bmpFileHeader.getSignatureBytes());
+  EXPECT_EQ(786486, bmpFileHeader.getSizeOfBitmapFile());
+  EXPECT_EQ(0, bmpFileHeader.getReservedBytes());
+  EXPECT_EQ(54, bmpFileHeader.getPixelDataOffset());
+}
+
+TEST(BitmapReaderTests, ShouldExtractBitmapInfoHeaderFromImage)
+{
+  char* bmpFile = (
+    "C:\\Users\\rmvir\\Desktop\\scc300-Win3D\\test\\output\\" \
+    "correct_resources\\blueImage512x512.bmp"
+  );
+
+  BitmapDibHeader bmpInfoHeader = BitmapReader::getBitmapDibHeader(bmpFile);
+
+  EXPECT_EQ(40, bmpInfoHeader.getSizeOfHeaderInBytes());
+  EXPECT_EQ(512, bmpInfoHeader.getWidthInPixels());
+  EXPECT_EQ(512, bmpInfoHeader.getHeightInPixels());
+  EXPECT_EQ(1, bmpInfoHeader.getNumberOfColorPlanes());
+  EXPECT_EQ(0, bmpInfoHeader.getCompressionMethod());
+  EXPECT_EQ(0, bmpInfoHeader.getRawBitmapDataSize());
+  EXPECT_EQ(0, bmpInfoHeader.getHorizontalPixelsPerMetre());
+  EXPECT_EQ(0, bmpInfoHeader.getVerticalPixelsPerMetre());
+  EXPECT_EQ(0, bmpInfoHeader.getColorTableEntries());
+  EXPECT_EQ(0, bmpInfoHeader.getImportantColors());
+}
+
+TEST(BitmapReaderTests, ShouldGetPixelArraySizeFromImage)
 {
   char* bitmapFile = (
     "C:\\Users\\rmvir\\Desktop\\scc300-Win3D\\test\\output\\" \
