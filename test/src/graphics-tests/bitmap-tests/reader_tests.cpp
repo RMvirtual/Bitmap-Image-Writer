bca4051b52b686b8bd4c00e2e2e1a3ca899011d0
RMvirtual <rmvirtual@AOL.com>
2021-08-29 13:05:24 +0100

    BitmapReaderTests: Refactored file path declarations for the blue test image.

--- test/src/graphics-tests/bitmap-tests/reader_tests.cpp
+++ test/src/graphics-tests/bitmap-tests/reader_tests.cpp
@@ -4,6 +4,7 @@
 #include "src/graphics/bitmaps/packet/pixels/pixel_array.h"
 #include "src/graphics/bitmaps/reader/bitmap_reader.h"
 #include "src/graphics/bitmaps/bitmap_image.h"
+#include "test/src/graphics-tests/bitmap-tests/bitmap_image_comparators.hpp"
 
 BitmapFileHeader setUpBluePixelFileHeader()
 {
@@ -56,74 +57,18 @@ BitmapImage setUpBluePixelBitmapImage()
   return bitmapImage;
 }
 
-void compareBitmapFileHeaders(
-  BitmapFileHeader header1, BitmapFileHeader header2)
+std::string getBlueImagePath()
 {
-  EXPECT_EQ(header1.getSignatureBytes()[0], header2.getSignatureBytes()[0]);
-  EXPECT_EQ(header1.getSignatureBytes()[1], header2.getSignatureBytes()[1]);
-  EXPECT_EQ(header1.getSizeOfBitmapFile(), header2.getSizeOfBitmapFile());
-  EXPECT_EQ(header1.getReservedBytes(), header2.getReservedBytes());
-  EXPECT_EQ(header2.getPixelDataOffset(), header2.getPixelDataOffset());
-}
+  std::string imagePath = 
+    "C:\\Users\\rmvir\\Desktop\\scc300-Win3D\\test\\output\\" \
+    "correct_resources\\blueImage512x512.bmp";
 
-void compareBitmapDibHeaders(
-  BitmapDibHeader header1, BitmapDibHeader header2)
-{
-  EXPECT_EQ(
-    header1.getSizeOfHeaderInBytes(), header2.getSizeOfHeaderInBytes());
-
-  EXPECT_EQ(header1.getWidthInPixels(), header2.getWidthInPixels());
-  EXPECT_EQ(header1.getHeightInPixels(), header2.getHeightInPixels());
-  EXPECT_EQ(
-    header1.getNumberOfColorPlanes(), header2.getNumberOfColorPlanes());
-
-  EXPECT_EQ(header1.getColorDepth(), header2.getColorDepth());
-  EXPECT_EQ(header1.getCompressionMethod(), header2.getCompressionMethod());
-  EXPECT_EQ(header1.getRawBitmapDataSize(), header2.getRawBitmapDataSize());
-  EXPECT_EQ(
-    header1.getHorizontalPixelsPerMetre(),
-    header2.getHorizontalPixelsPerMetre()
-  );
-
-  EXPECT_EQ(
-    header1.getVerticalPixelsPerMetre(), header2.getVerticalPixelsPerMetre());
-
-  EXPECT_EQ(header1.getColorTableEntries(), header2.getColorTableEntries());
-  EXPECT_EQ(header1.getImportantColors(), header2.getImportantColors());
-}
-
-void comparePixelArrays(PixelArray pixelArray1, PixelArray pixelArray2)
-{
-  EXPECT_EQ(pixelArray1.getHeightInPixels(), pixelArray2.getHeightInPixels());
-  EXPECT_EQ(pixelArray1.getWidthInPixels(), pixelArray2.getWidthInPixels());
-  EXPECT_EQ(pixelArray1.sizeInPixels(), pixelArray2.sizeInPixels());
-  EXPECT_EQ(pixelArray1.sizeInBytes(), pixelArray2.sizeInBytes());
-
-  int numberOfPixels = pixelArray1.sizeInPixels();
-
-  for (int pixelNo = 0; pixelNo < numberOfPixels; pixelNo++) {
-    Pixel pixel1 = pixelArray1.pixels[pixelNo];
-    Pixel pixel2 = pixelArray2.pixels[pixelNo];
-
-    EXPECT_EQ(pixel1.getBlue(), pixel2.getBlue());
-    EXPECT_EQ(pixel1.getGreen(), pixel2.getGreen());
-    EXPECT_EQ(pixel1.getRed(), pixel2.getRed());
-  }
-}
-
-void compareBitmapImages(BitmapImage image1, BitmapImage image2)
-{
-  compareBitmapFileHeaders(image1.getFileHeader(), image2.getFileHeader());
-  compareBitmapDibHeaders(image1.getDibHeader(), image2.getDibHeader());
-  comparePixelArrays(image1.getPixelArray(), image2.getPixelArray());
+  return imagePath;
 }
 
 TEST(BitmapReaderTests, ShouldExtractFileHeaderFromFile)
 { 
-  char* imageToTest = (
-    "C:\\Users\\rmvir\\Desktop\\scc300-Win3D\\test\\output\\" \
-    "correct_resources\\blueImage512x512.bmp"
-  );
+  std::string imageToTest = getBlueImagePath();
 
   BitmapFileHeader fileHeader = BitmapReader::getBitmapFileHeader(imageToTest);
   BitmapFileHeader correctFileHeader = setUpBluePixelFileHeader();
@@ -133,12 +78,9 @@ TEST(BitmapReaderTests, ShouldExtractFileHeaderFromFile)
 
 TEST(BitmapReaderTests, ShouldExtractDibHeaderFromFile)
 {
-  char* bmpFile = (
-    "C:\\Users\\rmvir\\Desktop\\scc300-Win3D\\test\\output\\" \
-    "correct_resources\\blueImage512x512.bmp"
-  );
+  std::string imageToTest = getBlueImagePath();
 
-  BitmapDibHeader dibHeader = BitmapReader::getBitmapDibHeader(bmpFile);
+  BitmapDibHeader dibHeader = BitmapReader::getBitmapDibHeader(imageToTest);
   BitmapDibHeader correctDibHeader = setUpBluePixelDibHeader();
 
   compareBitmapDibHeaders(correctDibHeader, dibHeader);
@@ -146,10 +88,7 @@ TEST(BitmapReaderTests, ShouldExtractDibHeaderFromFile)
 
 TEST(BitmapReaderTests, ShouldExtractPixelArrayFromFile)
 {
-  char* imageToTest = (
-    "C:\\Users\\rmvir\\Desktop\\scc300-Win3D\\test\\output\\" \
-    "correct_resources\\blueImage512x512.bmp"
-  );
+  std::string imageToTest = getBlueImagePath();
 
   PixelArray pixelArrayToTest = BitmapReader::getPixelArray(imageToTest);
   PixelArray correctPixelArray = setUpBluePixelArray();
@@ -159,22 +98,18 @@ TEST(BitmapReaderTests, ShouldExtractPixelArrayFromFile)
 
 TEST(BitmapReaderTests, ShouldExtractPixelArraySizeFromFile)
 {
-  char* bitmapFile = (
-    "C:\\Users\\rmvir\\Desktop\\scc300-Win3D\\test\\output\\" \
-    "correct_resources\\blueImage512x512.bmp"
-  );
+  std::string imageToTest = getBlueImagePath();
 
   int correctSize = 786432;
-  int payloadSizeInBytes = BitmapReader::getPixelArraySizeInBytes(bitmapFile);
+  int payloadSizeInBytes = BitmapReader::getPixelArraySizeInBytes(
+    imageToTest);
 
   EXPECT_EQ(correctSize, payloadSizeInBytes);
 }
 
 TEST(BitmapReaderTests, ShouldExtractBitmapImageObjectFromFile)
 {
-  char* imagePath =
-    "C:\\Users\\rmvir\\Desktop\\scc300-Win3D\\test\\output\\" \
-    "correct_resources\\blueImage512x512.bmp";
+  std::string imagePath = getBlueImagePath();
 
   BitmapImage imageToTest = BitmapImage::fromFile(imagePath);
   BitmapImage correctImage = setUpBluePixelBitmapImage();
