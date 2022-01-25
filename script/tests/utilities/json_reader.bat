ed93f42cb68b7dc810983380b311c76a5d6f97e3
RMvirtual <rmvirtual@AOL.com>
2022-01-25 20:10:22 +0000

    Renamed test scripts for shorter names. Set up failing json reader test.

---
+++ script/tests/utilities/json_reader.bat
@@ -0,0 +1,1 @@
+bazel test --test_output=all --test_summary=terse //test/src/utilities-tests:json_reader_tests
---
+++ test/src/utilities-tests/json_reader_tests.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include "src/utilities/json_reader.h"
+
+TEST(JSONReaderTests, ShouldInitialiseJSONReader)
+{
+  EXPECT_TRUE(false);
+}
+
+
