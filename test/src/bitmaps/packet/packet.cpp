95c4aefaabfd1cf3094697460979f51b311dba31
RMvirtual <rmvirtual@AOL.com>
2022-02-04 12:49:17 +0000

    Added test for filling pixel array.

--- test/src/bitmaps/packet/packet.cpp
+++ test/src/bitmaps/packet/packet.cpp
@@ -1,5 +1,4 @@
 #include <gtest/gtest.h>
-
 #include "test/src/bitmaps/packet/fixture.h"
 
 TEST_F(BitmapPacketTest, ShouldSetPixelInPixelArray)
@@ -20,4 +19,18 @@ TEST_F(BitmapPacketTest, ShouldGetNumberOfPixelsInPixelArray)
   int actualNumberOfPixels = this->pixelArray.sizeInPixels();
 
   EXPECT_EQ(correctNumberOfPixels, actualNumberOfPixels);
+}
+
+TEST_F(BitmapPacketTest, ShouldFillPixelArray)
+{
+  this->loadRedPixelArray();
+  auto greenColours = this->greenColours();
+  this->pixelArray.fill(greenColours);
+
+  int noOfPixels = this->pixelArray.sizeInPixels();
+
+  for (int i = 0; i < noOfPixels; i++) {
+    auto pixel = this->pixelArray.at(i);
+    this->isGreen(pixel);
+  }
 }
