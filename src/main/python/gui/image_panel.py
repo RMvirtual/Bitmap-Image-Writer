f6c2a62f18cda0bb727249860313a48968f659b1
RMvirtual <rmvirtual@AOL.com>
2022-02-27 10:57:10 +0000

    More work on the GUI sizing.

--- src/main/python/gui/image_panel.py
+++ src/main/python/gui/image_panel.py
@@ -6,7 +6,26 @@ class ImagePanel(wx.Panel):
     def __init__(self, parent: wx.Frame) -> None:
         super().__init__(parent=parent)
 
-        self.startImage = wx.Bitmap(width=500, height=500)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.startImage = wx.Bitmap(width=400, height=400)
 
         self.bitmapCtrl = wx.StaticBitmap(
             self, wx.ID_ANY, self.startImage)
+
+        self.sizer.Add(
+            window=self.bitmapCtrl,
+            flags=wx.SizerFlags(1).Expand().Border(wx.ALL, 10)
+        )
+
+        self.instructLbl = wx.StaticText(
+            parent=self, 
+            label="Do stuff and things."
+        )
+
+        self.sizer.Add(
+            window=self.instructLbl,
+            flags=wx.SizerFlags(1).Expand().Border(wx.ALL, 10)
+        )
+
+        self.SetSizer(self.sizer)
--- src/main/python/gui/viewer.py
+++ src/main/python/gui/viewer.py
@@ -6,20 +6,12 @@ class GUI(wx.Frame):
 
     def __init__(self):
         super(GUI, self).__init__(None)
-        self.SetSize(width=500,height=500)
+        # self.SetSize(width=500,height=500)
         self.SetTitle("Win3D")
 
         self.sizer = wx.BoxSizer(wx.VERTICAL)
 
-        self.imagePanel = ImagePanel(self)
+        self.imagePanel = ImagePanel(self)                
+        self.sizer.Add(self.imagePanel)
 
-        self.sizer.Add(
-            wx.StaticLine(self.imagePanel, wx.ID_ANY),
-            0, wx.ALL|wx.EXPAND, 5
-        )
-
-        instructions = "Do stuff and things."
-        self.instructLbl = wx.StaticText(self.imagePanel, label=instructions)
-        self.sizer.Add(self.instructLbl, 0, wx.ALL, 5)
-
-        self.imagePanel.SetSizer(self.sizer)
+        self.SetSizer(self.sizer)
