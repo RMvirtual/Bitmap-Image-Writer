e4eb2c5266c004f43bd2b0833fa3954a1d2ed58d
RMvirtual <rmvirtual@AOL.com>
2022-03-03 04:03:23 +0000

    Added draw function to rasteriser test.

--- src/test/cpp/rasterisation/tests.cpp
+++ src/test/cpp/rasterisation/tests.cpp
@@ -13,7 +13,15 @@ TEST_F(RasterisationTest, ShouldDrawTriangle)
   format.setWidthInPixels(500);
 
   auto image = std::make_shared<Bitmaps::Image>(format);
+  
+  auto redColours = format.colours();
+  redColours["red"] = 255;
+  redColours["green"] = 0;
+  redColours["blue"] = 0;
+
+  image->fill(redColours);
   this->rasteriser.setWritableImage(image);
+  this->rasteriser.draw(triangle);
 
   EXPECT_TRUE(false);
 }
