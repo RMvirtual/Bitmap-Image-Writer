feb5f9f03f30728082c5d3d2c9a3e1b475900022
RMvirtual <rmvirtual@AOL.com>
2022-03-04 17:48:11 +0000

    Even shorter debugging printouts.

--- src/test/cpp/rasterisation/plotters/line/asserts.cpp
+++ src/test/cpp/rasterisation/plotters/line/asserts.cpp
@@ -8,11 +8,26 @@ void LinePlotterTest::compare(std::vector<Maths::Vector> plotPoints)
 
   ASSERT_EQ(correctNoOfPlotPoints, noOfPlotPoints);
 
+  auto failureDetails = [](int pointNo, Maths::Vector correctPoint, Maths::Vector point) {
+    std::string failure = {
+      "Failed point No:" + std::to_string(pointNo)
+      + ". Received x:" + std::to_string(point["x"])
+      + " y:" + std::to_string(point["y"])
+      + ". Expected x:" + std::to_string(correctPoint["x"])
+      + " y:" + std::to_string(correctPoint["y"])
+    };
+
+    return failure;
+  };
+
   for (int pointNo = 0; pointNo < noOfPlotPoints; pointNo++) {    
     auto point = plotPoints[pointNo];
     auto correctPoint = this->correctPlotPoints[pointNo];
 
-    EXPECT_EQ(correctPoint["x"], point["x"]);
-    ASSERT_EQ(correctPoint["y"], point["y"]) << "Failed X: " << pointNo;
+    ASSERT_EQ(correctPoint["x"], point["x"]) << 
+      failureDetails(pointNo, correctPoint, point);
+    
+    ASSERT_EQ(correctPoint["y"], point["y"]) << 
+      failureDetails(pointNo, correctPoint, point);
   }
 }
