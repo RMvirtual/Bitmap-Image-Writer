34027e417da4565016dd45df5c17137d3dc9b587
RMvirtual <rmvirtual@AOL.com>
2022-03-05 00:48:15 +0000

    Added negative vector translation triangle tests.

--- src/test/cpp/geometry/triangle/tests.cpp
+++ src/test/cpp/geometry/triangle/tests.cpp
@@ -1,6 +1,6 @@
 #include "src/test/cpp/geometry/triangle/fixture.h"
 
-TEST_F(TriangleTest, ShouldTranslateTriangleFromZero)
+TEST_F(TriangleTest, ShouldTranslateTrianglePositiveFromZeroOrigin)
 {
   Maths::Vector translation {3, 3};
 
@@ -25,6 +25,31 @@ TEST_F(TriangleTest, ShouldTranslateTriangleFromZero)
   }
 }
 
+TEST_F(TriangleTest, ShouldTranslateTriangleNegativeFromZeroOrigin)
+{
+  Maths::Vector translation {-3, -3};
+
+  std::vector<Maths::Vector> v = {
+    {0.0, 0.0}, {1.0, 1.0}, {2.0, 0.0}
+  };
+
+  this->triangle = {v};
+
+  this->triangle.translate(translation);
+
+  std::vector<Maths::Vector> correctVectors = {
+    {-3,-3}, {-2,-2}, {-1,-3}
+  };
+
+  for (int i = 0; i < 3; i++) {
+    auto vector = this->triangle[i];
+    auto correctVector = correctVectors[i];
+
+    EXPECT_EQ(correctVector["x"], vector["x"]);
+    EXPECT_EQ(correctVector["y"], vector["y"]);
+  }
+}
+
 TEST_F(TriangleTest, ShouldTranslateTriangleFromNonZero)
 {
   Maths::Vector translation {3, 3};
@@ -38,6 +63,30 @@ TEST_F(TriangleTest, ShouldTranslateTriangleFromNonZero)
 
   std::vector<Maths::Vector> correctVectors = {
     {8,8}, {13,18}, {18,10}
+  };
+
+  for (int i = 0; i < 3; i++) {
+    auto vector = this->triangle[i];
+    auto correctVector = correctVectors[i];
+
+    EXPECT_EQ(correctVector["x"], vector["x"]);
+    EXPECT_EQ(correctVector["y"], vector["y"]);
+  }
+}
+
+TEST_F(TriangleTest, ShouldTranslateTriangleNegativeFromNonZero)
+{
+  Maths::Vector translation {-3, -3};
+
+  std::vector<Maths::Vector> v = {
+    {5, 5}, {10, 15}, {15, 7}
+  };
+
+  this->triangle = {v};
+  this->triangle.translate(translation);
+
+  std::vector<Maths::Vector> correctVectors = {
+    {2,2}, {7,12}, {12,4}
   };
 
   for (int i = 0; i < 3; i++) {
