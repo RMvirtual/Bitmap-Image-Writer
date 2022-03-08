0f13183d97821ecce3bee8ccdf7c8145ee00a1cc
RMvirtual <rmvirtual@AOL.com>
2022-03-08 21:09:46 +0000

    Letter F added.

--- src/test/cpp/text/textbox/tests.cpp
+++ src/test/cpp/text/textbox/tests.cpp
@@ -130,7 +130,7 @@ TEST_F(TextTest, ShouldCreateTextImageFile)
 {
   auto image = this->redImage();
 
-  Text::Text2D text {"abcdeh"};
+  Text::Text2D text {"abcdefh"};
   text.setPadding(0.25, 0.0);
   text.scale(20);
   text.translate({25, 100});
