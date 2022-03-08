87a1f80843753854592d08bcf503e3438a2276ad
RMvirtual <rmvirtual@AOL.com>
2022-03-08 23:33:07 +0000

    Started argument parser for demo application. Thanks Matt Broadbent.

--- src/main/python/app/gui.py
+++ src/main/python/app/gui.py
@@ -1,7 +1,8 @@
+import argparse
 import wx
 from src.main.python.gui.viewer import GUI
 
-def main():
+def main(args):
     app = wx.App()
 
     gui = GUI()
@@ -16,5 +17,18 @@ def main():
     gui.setText(imagePath)
     app.MainLoop()
 
+def setupArgumentParser() -> argparse.Namespace:
+    """Takes arguments from the command line to run several demoes by the
+    user."""
+
+    parser = argparse.ArgumentParser(description=(
+        "A collection of demos for demonstrating Win3D."))
+    
+    parser.set_defaults(func=main)
+    args = parser.parse_args()
+    
+    return args
+
 if __name__ == "__main__":
-    main()
+    args = setupArgumentParser()
+    args.func(args)
