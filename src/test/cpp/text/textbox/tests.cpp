d233e8ccae13b1306456b40af507defb055f7810
RMvirtual <rmvirtual@AOL.com>
2022-03-08 21:29:12 +0000

    Added letter K.

--- src/test/cpp/text/textbox/tests.cpp
+++ src/test/cpp/text/textbox/tests.cpp
@@ -130,7 +130,7 @@ TEST_F(TextTest, ShouldCreateTextImageFile)
 {
   auto image = this->redImage();
 
-  Text::Text2D text {"abcdefghij"};
+  Text::Text2D text {"abcdefghijk"};
   text.setPadding(0.25, 0.0);
   text.scale(20);
   text.translate({25, 100});
