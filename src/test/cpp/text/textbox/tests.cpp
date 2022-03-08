d559c616fd1eb12e1e46ea34ead80b9826c7a9a3
RMvirtual <rmvirtual@AOL.com>
2022-03-08 21:14:52 +0000

    Added letter G.

--- src/test/cpp/text/textbox/tests.cpp
+++ src/test/cpp/text/textbox/tests.cpp
@@ -130,8 +130,8 @@ TEST_F(TextTest, ShouldCreateTextImageFile)
 {
   auto image = this->redImage();
 
-  Text::Text2D text {"abcdefh"};
-  text.setPadding(0.25, 0.0);
+  Text::Text2D text {"abcdefgh"};
+  text.setPadding(0.25, 0.0); // thing.
   text.scale(20);
   text.translate({25, 100});
   this->rasteriser.setWritableImage(image);
