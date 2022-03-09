f4d1562d3824aed5a9444adedec0159809dca65b
RMvirtual <rmvirtual@AOL.com>
2022-03-09 11:20:43 +0000

    Passes compile test for GUI interface.£

--- src/main/cpp/gui/python_interface.cpp
+++ src/main/cpp/gui/python_interface.cpp
@@ -1,4 +1,5 @@
 #include "src/main/cpp/gui/python_interface.h"
+#include "python.h"
 
 GUI::GUIInterface::GUIInterface()
 {
--- src/main/cpp/gui/python_interface.h
+++ src/main/cpp/gui/python_interface.h
@@ -1,5 +1,3 @@
-#include "python.h"
-
 namespace GUI {
 class GUIInterface
 {
